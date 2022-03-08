dcf4895e7ac6b37152a05f41f83e03d631234206
nedzie <marcos-elivelton@hotmail.com>
2022-03-08 15:32:23 -0300

    Add project files.

---
+++ ExercicioQuatro.ConsoleApp/Carro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioQuatro.ConsoleApp
+{
+    internal class Carro
+    {
+        public int quilometragemInicial;
+        public int quilometragemFinal;
+        public double combustivelConsumido;
+        public double consumo;
+
+        public double CalcularConsumo(int quilometragemInicial, int quilometragemFinal, double combustivelConsumido)
+        {
+            consumo = (quilometragemFinal - quilometragemInicial) / combustivelConsumido;
+            return consumo;
+        }
+    }
+}
---
+++ ExercicioQuatro.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioQuatro.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro siena = new Carro();
+            Console.Write("Informe os km iniciais: \n> ");
+            siena.quilometragemInicial = int.Parse(Console.ReadLine());
+            Console.Write("Informe os km finais: \n> ");
+            siena.quilometragemFinal = int.Parse(Console.ReadLine());
+            Console.Write("Informe quantos litros de combustível foram gastos: \n> ");
+            siena.combustivelConsumido = double.Parse(Console.ReadLine());
+
+            siena.consumo = siena.CalcularConsumo(siena.quilometragemInicial, siena.quilometragemFinal, siena.combustivelConsumido);
+
+            Console.WriteLine("Nesta viagem, foram consumidos " + siena.consumo + " litros de combustível");
+            Console.ReadKey();
+        }
+    }
+}
